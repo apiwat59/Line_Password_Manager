a57caff2e2f479faa7e87d1168eee97ece6c5c7a
Wutthinan <wutthinan@odd.works>
2019-03-04 17:26:26 +0700

    Rename file

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-  LINE_TOKEN: 'linetoken input here', // ใส่ Channel access token
+  LINE_TOKEN: '', // ใส่ Channel access token
   LINE_API: 'https://api.line.me/v2/bot/message',
   FIREBASE: {
-    PROJECT_ID: 'projectId Firebase input here', // ใส่ projectId fore Firebase
+    PROJECT_ID: '', // ใส่ projectId fore Firebase
     KEY_PATH: './src/key.json' // ที่อยู่ของ key json ของ firebase ไฟล์
   },
   URL_COPY_PASSWORD: 'https://example.com/v1/api/copy?text=', // แก้ไข url ที่ deploy เพื่อใช้งานจริงได้เลย
