09e3b4e0aed8499b7be82fbe36535d669874972c
Wutthinan <wutthinan@odd.works>
2019-03-05 10:59:06 +0700

    Change url

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ module.exports = {
     PROJECT_ID: process.env.LINE_BOT_FIRE,
     KEY_PATH: './src/key.json'
   },
-  URL_COPY_PASSWORD: 'https://f3cce102.ngrok.io/api/v1/copy?text=',
+  URL_COPY_PASSWORD: 'http://psmlinebotprod.herokuapp.com/api/v1/copy?text=',
   URL_LOGO: 'https://images2.imgbox.com/7e/0b/Cz8AV24h_o.png',
   BOT_MSG: {
     ACCOUNT: 'กรุณาระบุชื่อ Account ที่ต้องการ',
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,7 @@ let Firestore = require('@google-cloud/firestore')
 const {
   FIREBASE
 } = require('../constants')
+
 let firestore = new Firestore({
   projectId: FIREBASE.PROJECT_ID,
   keyFilename: FIREBASE.KEY_PATH
